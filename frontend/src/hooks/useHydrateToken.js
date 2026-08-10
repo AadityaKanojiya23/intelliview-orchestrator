f1385ee2257aaa5e66f3a3e2556a9167fa75c236
ce24adityakanojiya-maker <ce24.aditya.kanojiya@kccemsr.edu.in>
2026-08-11 00:04:05 +0530

    Update useHydrateToken.js

--- frontend/src/hooks/useHydrateToken.js
+++ frontend/src/hooks/useHydrateToken.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { useAppStore } from "@/lib/store";
 function useHydrateToken() {
-  const { hydrate, hasHydrated } = useAppStore();
+  const { hydrate, hasHydrated } = useAppStore();    
   useEffect(() => {
     if (!hasHydrated) hydrate();
   }, [hydrate, hasHydrated]);
