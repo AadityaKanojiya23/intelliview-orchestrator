e70f25ce4eb11f2710b0f8c9f76d3c3919b527fd
mrohini2911 <mrohini2911@gmail.com>
2026-08-17 05:12:50 +0530

    fix: merge alembic migration heads

---
+++ alembic/versions/b39909f765ee_merge_template_domain_and_candidate_.py
@@ -0,0 +1,28 @@
+"""merge template domain and candidate demographics migrations
+
+Revision ID: b39909f765ee
+Revises: 003_add_template_domain, ba062b2def4d
+Create Date: 2026-08-17 05:09:30.784161
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b39909f765ee'
+down_revision: Union[str, Sequence[str], None] = ('003_add_template_domain', 'ba062b2def4d')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
